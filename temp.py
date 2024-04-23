5bc2da238e95df1fb0f45664537accda4c3d8c3c
Matteo Massa <matteo.massa@studenti.isissgobetti.it>
2024-04-23 11:16:00 +0200

    commit 2

--- temp.py
+++ temp.py
@@ -1,2 +1,8 @@
 python("ciao")
 # untracked
+
+# git config --global user.email "you@example.com"
+# git config --global user.name "Your Name"
+
+# git config --global user.email "matteo.massa@studenti.isissgobetti.it"
+# git config --global user.name "Matteo Massa"
